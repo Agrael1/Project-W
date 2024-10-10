c190e594bee58f9f2b7ac0495d7e6e295ded26b1
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2024-10-10 11:56:25 +0200

    stupid ref

--- game/src/app.cpp
+++ game/src/app.cpp
@@ -2,6 +2,6 @@
 
 w::action<void> ut::app::init_async()
 {
-    auto&&[r,w] = co_await ut::create_window_async("Game", 1920, 1080, false);
+    auto[r,w] = co_await ut::create_window_async("Game", 1920, 1080, false);
     wnd = std::move(w);
 }
